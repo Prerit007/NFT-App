9a43299d06166f860454a97b2af8ab306df069d0
Prerit007 <75621370+Prerit007@users.noreply.github.com>
2022-07-01 15:21:02 +0530

    commit

--- App.js
+++ App.js
@@ -1,21 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import Home from "./screens/Home";
+import Details from "./screens/Details";
 
-export default function App() {
+const Stack = createStackNavigator();
+const theme = {
+  ...DefaultTheme.colors,
+  backgroundColor: "transparent",
+};
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer theme={theme}>
+      <Stack.Navigator
+        screenOptions={{ headerShown: false }}
+        initialRouteName="Home"
+      >
+        <Stack.Screen component={Home} name="Home" />
+        <Stack.Screen component={Details} name="Details" />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
---
+++ screens/Details.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Text } from "react-native";
+
+const details = () => {
+  <Text>Details</Text>;
+};
+
+export default details;
