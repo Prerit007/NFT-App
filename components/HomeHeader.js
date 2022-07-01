21dbd47a72bff5487a69636921749231312df076
Prerit007 <75621370+Prerit007@users.noreply.github.com>
2022-07-01 23:41:02 +0530

    commit

--- components/HomeHeader.js
+++ components/HomeHeader.js
@@ -8,28 +8,39 @@ const Homehead = () => {
         padding: SIZES.font,
       }}
     >
-      <View style={{ flexDirection:"row",
-    justifyContent:"space-between",
-    alignItems: "center" }}>
-      <Image
-      source = {assets.logo}
-      resizeMode="contain"
-      style={{width: 90, height:30}}/>
-
-      <View style={{width: 45, height: 45}}>
-        <Image 
-        source = {assets.person02}
-        resizeMode="contain"
-        style={{width: "100%", height: "100%"}}/>
-
-        <Image 
-        source = {assets.badge}
-        resizeMode="contain"
+      <View
         style={{
-          position: "absolute", width:15px, height:15px, bottom:0, right:0,}}/>
-      </View>
-    </View>
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <Image
+          source={assets.logo}
+          resizeMode="contain"
+          style={{ width: 90, height: 30 }}
+        />
 
+        <View style={{ width: 45, height: 45 }}>
+          <Image
+            source={assets.person02}
+            resizeMode="contain"
+            style={{ width: "100%", height: "100%" }}
+          />
+
+          <Image
+            source={assets.badge}
+            resizeMode="contain"
+            style={{
+              position: "absolute",
+              width: 15,
+              height: 15,
+              bottom: 0,
+              right: 0,
+            }}
+          />
+        </View>
+      </View>
     </View>
   );
 };
